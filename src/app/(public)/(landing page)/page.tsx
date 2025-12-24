acab3c9dd52e9be15db70d63c2fcbf327ce7db99
Vinícius Blandy <Viny19381@hotmail.com>
2025-12-24 13:41:26 -0300

    feat: create a step-by-step and responsive section.

--- src/app/(public)/(landing page)/page.tsx
+++ src/app/(public)/(landing page)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/landing-page/hero";
 import ProductVideo from "@/components/landing-page/product-video";
+import StepByStep from "@/components/landing-page/step-by-step";
 
 export default function LandingPage() {
   return (
     <div>
       <Hero />
       <ProductVideo />
+      <StepByStep />
     </div>
   );
 }
